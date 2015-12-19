d6fa5cbc826f40700aa7d9cc11aac74a660044b0
AugustRush <liupingwei30@gmail.com>
2015-12-19 21:09:22 +0800

    new first commit

---
+++ ARNewSegmentPager-swift/ARNewSegmentPager-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARNewSegmentPager-swift
+//
+//  Created by AugustRush on 12/19/15.
+//  Copyright © 2015 AugustRush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
